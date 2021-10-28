10d28c4307d94e4e5deb140fafe1eba79f4fcc1d
reynaldsauro <naldsauro@gmail.com>
2021-10-28 01:41:19 -0700

    first commit

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,10 @@
+
+
+<li class="nav-item">
+    <a href="{{ route('airPlaneFlights.index') }}"
+       class="nav-link {{ Request::is('airPlaneFlights*') ? 'active' : '' }}">
+        <p>Air Plane Flights</p>
+    </a>
+</li>
+
+
